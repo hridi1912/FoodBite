b211541aa63e1240cd1772302461824c4002c85a
hridi1912 <hriditaalam1@gmail.com>
2024-07-05 02:51:16 +0600

    made some changes in database

--- backend/controllers/foodController.js
+++
@@ -1,8 +0,0 @@
-import foodModel from "../models/foodmodel.js";
-import fs from 'fs'
-
-//add food items
-const addFood =async (req,res)=>{
-
-}
-export {addFood}
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
  import express from "express"
  import cors from "cors"
 import { connectDB } from "./config/db.js"
-import foodRouter from "./routes/foodRoute.js"
+
 import userRouter from "./routes/userRoute.js"
 import 'dotenv/config'
 
@@ -16,7 +16,7 @@ app.use(cors())
 // db connection
 connectDB();
 //api endpoint 
-app.use("/api/food",foodRouter)
+//app.use("/api/food",foodRouter)
 app.use("/api/user",userRouter)
 
 app.get("/",(req,res)=>{
