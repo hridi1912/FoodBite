d46f3bb098bff55cd81caaae07dfd09a0e2c70be
hridi1912 <hriditaalam1@gmail.com>
2024-07-11 19:31:08 +0600

    mongodb

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB =async()=>{
-    await mongoose.connect("mongodb+srv://hridita:<password>@foodbite.en9kbch.mongodb.net/").then(()=>console.log("DB connected"));
+    await mongoose.connect("mongodb+srv://hridita:secret123@foodbite.en9kbch.mongodb.net/").then(()=>console.log("DB connected"));
 }
