ece928f3961f8e73c012e8405dc3c67896414e4a
hridi1912 <hriditaalam1@gmail.com>
2024-07-15 10:42:41 +0600

    new change12

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,19 @@
-import mongoose from "mongoose";
+/*import mongoose from "mongoose";
 
 export const connectDB =async()=>{
     await mongoose.connect("mongodb+srv://hridita:secret123@foodbite.en9kbch.mongodb.net/FoodBite?retryWrites=true&w=majority&appName=FoodBite").then(()=>console.log("DB connected"));
-}
+}*/
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb+srv://hridita:secret123@foodbite.en9kbch.mongodb.net/FoodBite?retryWrites=true&w=majority&appName=FoodBite").then(()=>console.log("DB connected"));
+
+    console.log('MongoDB connected...');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+
