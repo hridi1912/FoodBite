6116223f37b59d8226b09d1ce52c223f869b8d68
hridi1912 <hriditaalam1@gmail.com>
2024-09-18 10:57:03 +0600

    try

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -30,7 +30,7 @@ const loginUser = async (req, res) => {
         }
         const token = createToken(user._id);
         const refreshToken = createRefreshToken(user._id);
-        res.json({data:user, success: true, token, refreshToken });
+        res.json({data:user, token, refreshToken, success: true,  });
         // res.status(200).json({
         //     message: "Login successfullyhgfhgf",
         //     data: {
